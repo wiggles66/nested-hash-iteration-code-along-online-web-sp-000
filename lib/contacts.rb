356ccb29e06a56d6512505b557d909720490058c
Michael Waide <mvwaide@gmail.com>
2020-07-03 20:02:55 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,15 +17,6 @@ require 'pry'
 
 
     def remove_strawberry(contacts)
-      contacts.each do |person, contact_details_hash|
-  if person == "Freddy Mercury"
-    contact_details_hash.each do |attribute, data|
-      if attribute == :favorite_ice_cream_flavors
-        data.delete_if {|ice_cream| ice_cream == "strawberry"}
-      end
-    end
-  end
-end
     contacts.each do |person, contact_details_hash|
       if person == "Freddy Mercury"
         contact_details_hash.each do |attribute, data|
